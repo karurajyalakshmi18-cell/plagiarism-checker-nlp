27ea9c2f38448f12a6e5b20cd7cc31643baa6700
24A31A4380 <karurajyalakshmi18@gmail.com>
2026-02-22 23:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-text1 = input("Enter first text: ")
-text2 = input("Enter second text: ")
+st.title("Plagiarism Checker (Mini NLP Project)")
 
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform([text1, text2])
+text1 = st.text_area("Enter First Text")
+text2 = st.text_area("Enter Second Text")
 
-similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-
-percentage = similarity[0][0] * 100
-
-print(f"Similarity Percentage: {percentage:.2f}%")
+if st.button("Check Similarity"):
+    if text1 and text2:
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform([text1, text2])
+        similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+        percentage = similarity[0][0] * 100
+        st.success(f"Similarity: {percentage:.2f}%")
+    else:
+        st.warning("Please enter both texts")
